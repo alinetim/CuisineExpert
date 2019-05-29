8926b6a6339b852ee1af15c133b16223ab8597d9
nata6602 <nata6602@gmail.com>
2019-05-29 16:50:01 +0300

    Added functions for restaurant search. Try/except would be added later.

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def change_x(x):
-    x += 10
-    return x
+from functions import get_id, find_cuisine, restaurant_search
 
+city = input('Enter a city: ')
+while True:
 
-change_x(10)
+    city_id = get_id(city)
+    cuisine = input('Enter a cuisine: ')
+    if not cuisine:
+        break
+    else:
+        cuisine_id_num = find_cuisine(cuisine, city_id)
+        result = restaurant_search(city_id, cuisine_id_num, f'restaurants_{cuisine}.txt')
