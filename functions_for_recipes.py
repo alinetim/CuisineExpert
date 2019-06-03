856f090706feb61db07298761b2a66eb670f6c30
valeriiaso <valeriya.strelnikova@gmail.com>
2019-06-03 15:00:00 +0300

    added main2

--- functions_for_recipes.py
+++ functions_for_recipes.py
@@ -34,7 +34,23 @@ def get_a_recipe(name_of_recipe):
             else:
                 print(data['meals'][INSTRUCTIONS][f'strIngredient{i}'], end='......')
                 print(data['meals'][INSTRUCTIONS][f'strMeasure{i}'])
+                ingredients.append(data['meals'][INSTRUCTIONS][f'strMeasure{i}'])
                 ingredients.append(data['meals'][INSTRUCTIONS][f'strIngredient{i}'])
                 i += 1
-        print(data['meals'][INSTRUCTIONS]['strInstructions'])
+        instruction = data['meals'][INSTRUCTIONS]['strInstructions']
+        instruction = instruction.split('.')
+        for sentence in instruction:
+            print(f'{sentence}.')
         return ingredients
+
+
+def food_text_analysis(recipe):
+
+    app_id = 'b25a621b'
+    app_key = '164b53282bbeda25555bac62065493ad'
+    urllib.parse.quote(recipe)
+
+    response = requests.get(f"https://api.edamam.com/api/nutrition-data?app_id={app_id}&app_key={app_key}&ingr={recipe}")
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
---
+++ main2.py
@@ -0,0 +1,14 @@
+from functions_for_recipes import get_a_recipe, get_all_possible_countries, get_recipes_by_area, food_text_analysis
+
+while True:
+    answer = input('Do you want to cook by yourself or go to restaurant?(Choose: recipe/ restaurant):')
+    if answer == 'recipe':
+        get_all_possible_countries()
+        country = input('Enter preferable country:')
+        get_recipes_by_area(country)
+        recipe = input('Choose recipe:')
+        ingredients = get_a_recipe(recipe)
+        string_ingr = ' '.join(ingredients)
+        print(string_ingr)
+        food_text_analysis(string_ingr)
+
