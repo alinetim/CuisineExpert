04271175bcd9f19c7d73f52807ab6c89e5f3a3f3
Наталья Курданова <nata6602@gmail.com>
2019-06-05 12:50:25 +0300

    reorganisation of main

--- main2.py
+++ main2.py
@@ -1,8 +1,11 @@
 from functions_for_recipes import get_a_recipe, get_all_possible_countries, get_recipes_by_area, food_text_analysis
+from functions_for_restaurants import get_id, find_cuisine, restaurant_search
 
 while True:
     answer = input('Do you want to cook by yourself or go to restaurant?(Choose: recipe/ restaurant):')
-    if answer == 'recipe':
+    if not answer:
+        break
+    elif answer == 'recipe':
         get_all_possible_countries()
         country = input('Enter preferable country:')
         get_recipes_by_area(country)
@@ -38,3 +41,26 @@ while True:
                 pass
             i += 2
         print(total_calories)
+    elif answer == 'restaurant':
+        city = input('Enter a city: ')
+        while True:
+
+            city_id = get_id(city)
+            cuisine = input('Enter a cuisine: ')
+            if not cuisine:
+                break
+            else:
+                cuisine_id_num = find_cuisine(cuisine, city_id)
+                restaurants = restaurant_search(city_id, cuisine_id_num, f'restaurants_{cuisine}.txt')
+
+                with open(path, 'w') as f:
+                    for i in range(len(restaurants['restaurants'])):
+                        f.write(f"Name: {restaurants['restaurants'][i]['restaurant']['name']}\n"
+                        f"Address: {restaurants['restaurants'][i]['restaurant']['location']['address']}\n"
+                        f"Average cost for two: {restaurants['restaurants'][i]['restaurant']['currency']}\n"
+                        f"{restaurants['restaurants'][i]['restaurant']['average_cost_for_two']}\n"
+                        f"Rating: {restaurants['restaurants'][i]['restaurant']['user_rating']['aggregate_rating']}\n"
+                        f"-----------------------------------------------------------------------\n")
+    else:
+        print('Wrong answer')
+        continue
